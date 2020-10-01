d2b08cc7a8729a51c2637a0da0b563075b395a20
HeloisaMMartins <helomoreiramartins@gmail.com>
2020-10-01 20:22:11 -0300

    alteraçãomenu

--- src/br/com/aula6/Menu.java
+++ src/br/com/aula6/Menu.java
@@ -3,8 +3,34 @@ package br.com.aula6;
 public class Menu {
 
 	public static void main(String[] args) {
-		
-		
+	Scanner scan = new Scanner(System.in);
+	String cadastras;
+	String logar;
+	String seLogado;
+	String opcao;
+	
+	System.out.println("Menu de opções:"
+			+ "\n1 - Cadastrar"
+			+ "\n2 - Logar"
+			+ "\n3 - Logado");
+	
+	opcao = scan.next();
+	
+	switch (opcao) {
+		case "1":
+			System.out.print("Cadastrar", cadastras);
+			break;
+		case "2";
+			System.out.print("Logar", logar);
+			break;
+		case "3";
+			System.out.print("Logado", Logado);	
+			
+		}
+	}
+}
+	
+	
 		// MENU
 		// CADASTRAR
 		// LOGAR
@@ -14,6 +40,6 @@ public class Menu {
 			// EXCLUIR CONTA
 			// SAIR
 
-	}
+	
 
-}
+
