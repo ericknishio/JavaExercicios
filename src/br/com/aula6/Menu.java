ae1b48e85592c8db5c3530ca6fbfb3b921ae406d
HeloisaMMartins <helomoreiramartins@gmail.com>
2020-10-01 21:26:18 -0300

    Projeto menu

--- src/br/com/aula6/Menu.java
+++ src/br/com/aula6/Menu.java
@@ -10,25 +10,62 @@ public class Menu {
 	String logar;
 	String seLogado;
 	String opcao;
+	String cpf;
+	String dataNascimento;
+	String email;
+	Integer idade;
+	String nome;
+	String rg;
+	String sexo;
+	String telefone;
 	
 	System.out.println("Menu de opções:"
-			+ "\n1 - Cadastrar"
-			+ "\n2 - Logar"
-			+ "\n3 - Logado");
+			+ "\n1 - Cadastrar Cliente"
+			+ "\n2 - Cadastrar Gerente"
+			+ "\n3 - Logar"
+			+ "\n4 - Exibir Cliente");
+			
 	
 	opcao = scan.next();
 	
-//	switch (opcao) {
-//		case "1":
-//			System.out.print("Cadastrar", cadastras);
-//			break;
-//		case "2";
-//			System.out.print("Logar", logar);
-//			break;
-//		case "3";
-//			System.out.print("Logado", Logado);	
+	
+	switch (opcao) {
+		case "1":
+			System.out.println("Informe nome: " );
+			nome = scan.next();
+			System.out.println("Informe CPF: " );
+			cpf = scan.next();
+			System.out.println("Informe RG: " );
+			rg = scan.next();
+			System.out.println("Informe Data de Nascimento: " );
+			dataNascimento = scan.next();
+			System.out.println("Informe email: " );
+			email = scan.next();
+			System.out.println("Informe telefone: " );
+			telefone = scan.next();
+			System.out.println("Informe idade: ");
+			idade = scan.nextInt();
+			System.out.println("Informe sexo: " );
+			sexo = scan.next();
+		break;
+		case "2":
 			
-//		}
+			break;
+		case "3":
+				
+		break;	
+		case "4":
+			Cadastro cadastro = new Cadastro();
+			System.out.println(cadastro.ExibirCliente());
+		break;
+		}
+	System.out.println("Menu de opções:"
+			+ "\n1 - Cadastrar Cliente"
+			+ "\n2 - Cadastrar Gerente"
+			+ "\n3 - Logar"
+			+ "\n4 - Exibir Cliente");
+			
+	opcao = scan.next();
 	}
 }
 	
